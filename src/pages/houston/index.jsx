5413ec7999a0d6d68fff82616095092140adaa24
lalitparashar06 <lalitparashar06@gmail.com>
2025-03-04 18:39:48 +0530

    Added Project

---
+++ src/pages/houston/index.jsx
@@ -0,0 +1,14 @@
+import DetailModule from "@/components/DetailModule";
+import ColThreeCard from "@/components/DetailModule";
+import HoustonImg from "@/components/HoustonImg";
+
+import React from "react";
+
+export default function Index() {
+  return (
+    <>
+      <DetailModule />
+      <HoustonImg />
+    </>
+  );
+}
